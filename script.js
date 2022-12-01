f06ebc2965a84c89ae462ec2ba032a6ce75641a5
BakhytSembayev <119586792+BakhytSembayev@users.noreply.github.com>
2022-12-01 16:33:29 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+const C = document.querySelector("canvas"),
+  $ = C.getContext("2d"),
+  W = (C.width = window.innerWidth),
+  H = (C.height = window.innerHeight);
+
+const str = "А+B0D-U1L=T2% &3З V4Q R5$ М6Н О7G H8C Т9E A!X W?X I.O@,S Ь:Ұ>;Z",
+  matrix = str.split("");
+
+let font = 14,
+  col = W / font,
+  arr = [];
+
+for (let i = 0; i < col; i++) arr[i] = 1;
+
+function draw() {
+  $.fillStyle = "rgba(0,0,0,.05)";
+  $.fillRect(0, 0, W, H);
+  $.fillStyle = "#0f0";
+  $.font = font + "px system-ui";
+  for (let i = 0; i < arr.length; i++) {
+    let txt = matrix[Math.floor(Math.random() * matrix.length)];
+    $.fillText(txt, i * font, arr[i] * font);
+    if (arr[i] * font > H && Math.random() > 0.975) arr[i] = 0;
+    arr[i]++;
+  }
+}
+
+setInterval(draw, 123);
+
+window.addEventListener("resize", () => location.reload());
